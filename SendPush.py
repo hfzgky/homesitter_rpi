f5821368077a172a0bd8f95b93bcb14302f952d1
ohsdy0114 <ohsdy0114@gmail.com>
2021-10-18 09:18:43 +0900

    url 가져다가 동영상 재생하고 푸시알림까지 성공

---
+++ SendPush.py
@@ -0,0 +1,4 @@
+import FCMManager as fcm
+
+tokens = ["c3QDY8soTkiV6x9hV65K8b:APA91bFs1ArB0kzZ_EVopFV2Uod6eG9Jh-Q9h1Hpa_20GTJoKYoGOlenMzojNskelAfa9J0XLmsyJr3HHrc81nscco_QF6H4y_iWYKYFZv8QhwKa6oEKBIbQ-ZJkgmTTGcUxeZZvm6hJ"]
+fcm.sendPush("Hi", "This is my next msg", tokens)
--- detect.py
+++ detect.py
@@ -6,11 +6,33 @@ from train_v2 import normalize,l2_normalizer
 from scipy.spatial.distance import cosine
 from tensorflow.keras.models import load_model
 import pickle
+from pyfcm import FCMNotification
+import FCMManager as fcm
 
+# 파베에 저장되어있는 1분컷 영상 가져오기 위한 변수
+count = 1
+
+# 최종적으로  unknown인지 아닌지 판별하기위한 변수
+global unknown_cnt
+unknown_cnt = 0
+global known_cnt
+known_cnt = 0
+
+
+
+APIKEY = "AAAA4a0r8r0:APA91bFOt0Xz4fkWgYeQumUeIU5GXTQZR3doAnAO0ObMdzgZZKT5402WiNPL3hCgnrLCaOdR-YzWJ0RygG-kBKtoX6LWlysiMDNmb4ELdWG7iLwoOMbC8vsjnftAHg7GAVvYssHiu-8z"
+# 애뮬 TOKEN = "fo0jBhQ9QaWasZHIvQNE4j:APA91bG7kn1ZTD2fZ0OO_s_VLDEGcZHh9gDYGEEZOhAABMd3f7SmXdleX2Y_RkzUEqxyjkfSLHveUqthaCQJctRErvCqLnzsgMi11Td_JADEZOSW9FmTMjor-1wxiJ_My-wlYO-h1R5M"
+tokens = ["c3QDY8soTkiV6x9hV65K8b:APA91bFs1ArB0kzZ_EVopFV2Uod6eG9Jh-Q9h1Hpa_20GTJoKYoGOlenMzojNskelAfa9J0XLmsyJr3HHrc81nscco_QF6H4y_iWYKYFZv8QhwKa6oEKBIbQ-ZJkgmTTGcUxeZZvm6hJ",
+          "fo0jBhQ9QaWasZHIvQNE4j:APA91bG7kn1ZTD2fZ0OO_s_VLDEGcZHh9gDYGEEZOhAABMd3f7SmXdleX2Y_RkzUEqxyjkfSLHveUqthaCQJctRErvCqLnzsgMi11Td_JADEZOSW9FmTMjor-1wxiJ_My-wlYO-h1R5M"]
+
+#  파이어베이스 콘솔에서 얻어온 서버키를 넣어줌
+push_service = FCMNotification(APIKEY)
 
 confidence_t=0.99
 recognition_t=0.5
 required_size = (160,160)
+
+
 
 def get_face(img, box):
     x1, y1, width, height = box
@@ -31,7 +53,10 @@ def load_pickle(path):
         encoding_dict = pickle.load(f)
     return encoding_dict
 
-def detect(img ,detector,encoder,encoding_dict):
+def detect(img, detector, encoder, encoding_dict):
+    global unknown_cnt
+    global known_cnt
+
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = detector.detect_faces(img_rgb)
     for res in results:
@@ -53,10 +78,14 @@ def detect(img ,detector,encoder,encoding_dict):
             cv2.rectangle(img, pt_1, pt_2, (0, 0, 255), 2)
             cv2.putText(img, name, pt_1, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
             print('너 누구야')
+            unknown_cnt += 1
+            print(unknown_cnt)
         else:
             cv2.rectangle(img, pt_1, pt_2, (0, 255, 0), 2)
             cv2.putText(img, name + f'__{distance:.2f}', (pt_1[0], pt_1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 200, 200), 2)
             print(name + "이 맞네요")
+            known_cnt += 1
+            print(known_cnt)
     return img 
 
 
@@ -69,23 +98,37 @@ if __name__ == "__main__":
     encodings_path = 'encodings/encodings.pkl'
     face_detector = mtcnn.MTCNN()
     encoding_dict = load_pickle(encodings_path)
-    
-    cap = cv2.VideoCapture('Zcon.mp4')
 
-    while cap.isOpened():
-        ret,frame = cap.read()
+    while True:
+        # cap = cv2.VideoCapture('Man.mp4') # <- 사람마다 바뀌어야 되는 부분
+        cap = cv2.VideoCapture('https://firebasestorage.googleapis.com/v0/b/homesitter-54d69.appspot.com/o/cctv%2FVideo%2F' + str(count) + '.mp4?alt=media&token=1') # <- 1분짜리영상마다 바뀌어야 되는 부분
 
-        if not ret:
-            print("CAM NOT OPEND") 
-            break
-        
-        frame= detect(frame , face_detector , face_encoder , encoding_dict)
+        while cap.isOpened():
+            ret,frame = cap.read()
 
-        cv2.imshow('camera', frame)
+            # 영상이 끝나면 실행
+            if not ret:
+                count += 1
+                break
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            frame= detect(frame , face_detector , face_encoder , encoding_dict)
 
-    
+            cv2.imshow('camera', frame)
 
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
+        if unknown_cnt > known_cnt:
+            fcm.sendPush("낯선사람", "등장등장", tokens)
+            print(unknown_cnt)
+            print(known_cnt)
+            print(str(count) + '번 동영상')
+            unknown_cnt = 0
+            known_cnt = 0
+        else:
+            print('주인이 맞다~~`~')
+            print(unknown_cnt)
+            print(known_cnt)
+            print(str(count) + '번 동영상')
+            unknown_cnt = 0
+            known_cnt = 0
